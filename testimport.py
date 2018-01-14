7482851b861e01b3879c5565be952aaaf0c764ed
xiaomaguoguo <xiaomaguoguo@gmail.com>
2018-01-14 12:12:32 +0800

    写文件

--- testimport.py
+++ testimport.py
@@ -1,3 +1,10 @@
 # import FirstPython
 from FirstPython import sum
 sum(10,20)
+# 打开一个文件
+f = open("/Users/KNothing/Desktop/foo.txt", "w")
+
+f.write( "Python 是一个非常好的语言。\n是的，的确非常好!!\n" )
+
+# 关闭打开的文件
+f.close()
